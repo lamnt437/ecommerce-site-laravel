01af5d556e0dbcc65314ab6ae9808a424455c88f
Lam Nguyen <lamnt437@gmail.com>
2019-11-10 12:05:00 +0700

    fix roleid default value in user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('role_id')->default(0);
+            $table->tinyInteger('role_id')->default(1);
             $table->rememberToken();
             $table->timestamps();
 
